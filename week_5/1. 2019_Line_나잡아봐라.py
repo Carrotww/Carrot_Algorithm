6a0f5971379dec98e1dec5a5abefa5997c669dd2
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-09-22 21:35:36 +0900

    commit

--- week_5/1. 2019_Line_나잡아봐라.py
+++ week_5/1. 2019_Line_나잡아봐라.py
@@ -41,7 +41,7 @@ def catch_me(cony, brown):
     return -1
 
 
-print(catch_me(c, b))  # 5가 나와야 합니다!
+print(catch_me(c, b))  # 5
 print("정답 = 3 / 현재 풀이 값 = ", catch_me(10,3))
 print("정답 = 8 / 현재 풀이 값 = ", catch_me(51,50))
 print("정답 = 28 / 현재 풀이 값 = ", catch_me(550,500))
