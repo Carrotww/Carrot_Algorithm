38bc2902cc15e304f1d57d449d1781a1a0b8e6a9
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-10-28 10:06:55 +0900

    camp algorithm 1week update

--- Algorithm_Question_types/BFS,DFS/2022_10_27_programmers_여행경로.py
+++ Algorithm_Question_types/BFS,DFS/2022_10_27_programmers_여행경로.py
@@ -1,5 +1,29 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/43164
 
+from collections import defaultdict
+
 def solution(tickets):
+    visited, path, stack = [], [], ["ICN"]
+    tic = defaultdict(list)
+    for a, b in tickets:
+        tic[a].append(b)
+    for t in tic.values():
+        t.sort(reverse=True)
     
-    return
+    def dfs(visited, path, stack):
+        if not stack:
+            print('done')
+            return path
+        start = stack.pop()
+        if start in tic:
+            for i in tic[start]:
+                if (start, i) not in visited:
+                    visited.append((start, i))
+                    stack.append(i)
+                    path.append(i)
+                    print(stack)
+                    dfs(visited, path, stack)
+    
+    result = dfs(visited, path, stack)
+    
+    return result
