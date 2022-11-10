02701ed61f932aaf498244e7c977d4df8fa9535e
Carrotww <rjgjfl90@naver.com>
2022-11-11 00:35:22 +0900

    algorithm camp study 2week - 6번까지

--- Algorithm_camp_study/2week/6_예산.py
+++ Algorithm_camp_study/2week/6_예산.py
@@ -1,2 +1,10 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12982
 
+def solution(d, budget):
+    d.sort()
+    for i in range(len(d)):
+        budget -= d[i]
+        if budget < 0:
+            return i
+    else:
+        return i + 1
