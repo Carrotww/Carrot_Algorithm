85542f4e1b03741fd0f4a370216921907ec67546
Carrotww <rjgjfl90@naver.com>
2022-11-14 20:06:32 +0900

    commit

--- My_Algorithm/None_Tech/22_11/2022_11_06_programmers_등대 3.py
+++ My_Algorithm/None_Tech/22_11/2022_11_06_programmers_등대 3.py
@@ -1,5 +1,5 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/133500
-from collections import defaultdict, deque
+from collections import defaultdict
 
 def solution(n, lighthouse):
     path_dict = defaultdict(list)
@@ -14,14 +14,49 @@ def solution(n, lighthouse):
     # result += 1을 한다
     
     result = 0
+    # while path_dict:
+    #     for end_node, side_node in path_dict.items():
+    #         if not side_node:
+    #             del(path_dict[end_node])
+    #         if len(side_node) == 1: # val 이 중심, key 가 끝 노드
+    #             del_val = [x for x in path_dict[side_node[0]]] # 끝에 달려있는거 저장
+    #             need_remove_node = side_node[0]
+    #             del(path_dict[side_node[0]]) # 중심 노드 제거 # 1
+    #             for key, val in path_dict.items():
+    #                 if need_remove_node in val:
+    #                     val.remove(need_remove_node)
+    #             result += 1
+    #             break
+    
+    # while path_dict:
+    #     for end_node, side_node in path_dict.items():
+    #         if len(side_node) == 1:
+    #             del_val = [x for x in path_dict[side_node[0]]]
+    #             need_remove_node = side_node[0]
+    #             result += 1
+    #             break
+    #     del_list = []
+    #     if need_remove_node:
+    #         del(path_dict[need_remove_node])
+    #     for key, val in path_dict.items():
+    #         if need_remove_node in val:
+    #             val.remove(need_remove_node)
+    #             if not val:
+    #                 del_list.append(key)
+    #     for dl in del_list:
+    #         del(path_dict[dl])
+    #     need_remove_node = 0
+
+    remove_list = []
     while path_dict:
         for end_node, side_node in path_dict.items():
-            if len(side_node) == 1: # val 이 중심, key 가 끝 노드
-                del_val = [x for x in path_dict[side_node[0]]] # 끝에 달려있는거 저장
-                del(path_dict[side_node[0]]) # 중심 노드 제거
-                
+            if len(side_node) == 1 and end_node in path_dict:
+                remove_list.append(side_node[0])
+                del(path_dict[side_node[0]])
+                result += 1
+        break
 
     return result
 
 print(solution(8, [[1, 2], [1, 3], [1, 4], [1, 5], [5, 6], [5, 7], [5, 8]]))
-print(solution(10, [[4, 1], [5, 1], [5, 6], [7, 6], [1, 2], [1, 3], [6, 8], [2, 9], [9, 10]]))
+# print(solution(10, [[4, 1], [5, 1], [5, 6], [7, 6], [1, 2], [1, 3], [6, 8], [2, 9], [9, 10]]))
