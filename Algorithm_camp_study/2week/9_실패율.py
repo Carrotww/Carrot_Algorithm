baf202bb07f702c91a7e9bb525e85c0383715b46
Carrotww <rjgjfl90@naver.com>
2022-11-17 15:53:49 +0900

    commit

--- Algorithm_camp_study/2week/9_실패율.py
+++ Algorithm_camp_study/2week/9_실패율.py
@@ -19,18 +19,3 @@ def solution(N, stages):
 print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]))
 print(solution(4, [4,4,4,4,4]))
 print(solution(8, [1, 2, 3, 4, 5, 6, 7]))
-
-# def solution(N, stages):
-#     result = {}
-#     total = len(stages)
-
-#     for i in range(1, N + 1):
-#         if total != 0:
-#             current_n = stages.count(i)
-#             result[i] = current_n / total
-#             total -= current_n
-#         else:
-#             result[i] = 0
-    
-#     result = sorted(result, key=lambda x :result[x], reverse=True)
-#     return result
