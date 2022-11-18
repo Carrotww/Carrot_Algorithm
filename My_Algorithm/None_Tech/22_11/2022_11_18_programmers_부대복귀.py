eba885668b93d0cfe5219fad31a2a4dfe557eedb
Carrotww <rjgjfl90@naver.com>
2022-11-18 10:48:42 +0900

    부대복귀 done

--- My_Algorithm/None_Tech/22_11/2022_11_18_programmers_부대복귀.py
+++ My_Algorithm/None_Tech/22_11/2022_11_18_programmers_부대복귀.py
@@ -1,6 +1,27 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/132266
 
 from collections import deque, defaultdict
+
+def solution(n, roads, sources, destination):
+    roads_path = defaultdict(list)
+    for a, b in roads:
+        roads_path[a].append(b)
+        roads_path[b].append(a)
+    
+    queue = deque([destination])
+    visited = {x:-1 for x in range(1, n+1)}
+    visited[destination] = 0
+    while queue:
+        cur_road = queue.popleft()
+        cur_dis = visited[cur_road]
+        for nxt_road in roads_path[cur_road]:
+            if visited[nxt_road] == -1:
+                visited[nxt_road] = cur_dis + 1
+                queue.append(nxt_road)
+    return [visited[x] for x in sources]
+
+'''
+from collections import deque, defaultdict
 import math
 
 def solution(n, roads, sources, destination):
@@ -28,6 +49,7 @@ def solution(n, roads, sources, destination):
         if visited[destination] == math.inf:
             result.append(-1)
     return result
+'''
 
 '''
 from collections import deque, defaultdict
