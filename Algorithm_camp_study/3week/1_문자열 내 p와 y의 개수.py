63f793d4205934835fe6c3619cb49994870e28c9
Carrotww <rjgjfl90@naver.com>
2022-11-29 23:22:09 +0900

    commit

--- Algorithm_camp_study/3week/1_문자열 내 p와 y의 개수.py
+++ Algorithm_camp_study/3week/1_문자열 내 p와 y의 개수.py
@@ -1,1 +1,7 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12916
+
+def solution(s):
+    result = s.lower()
+    if result.count('p') != result.count('y'):
+        return False
+    return True
