99b6e7474051e1a25aad5fdc51f9d92968c6e57e
Carrotww <rjgjfl90@naver.com>
2023-01-01 16:58:09 +0900

    23

--- My_Algorithm/None_Tech/2023/23_01/2023_01_01_programmers_기지국설치.py
+++ My_Algorithm/None_Tech/2023/23_01/2023_01_01_programmers_기지국설치.py
@@ -1,1 +1,9 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12979
+
+def solution(n, satations, w):
+    result = 0
+    
+    return result
+
+print(solution(11, [4, 11], 1))
+print(solution(16, [9], 2))
