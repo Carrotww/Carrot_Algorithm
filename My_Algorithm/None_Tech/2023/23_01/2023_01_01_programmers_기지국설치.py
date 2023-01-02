2e131fd4f1545110e1fe3a30d488d7f47326a787
Carrotww <rjgjfl90@naver.com>
2023-01-03 00:15:39 +0900

    temp

--- My_Algorithm/None_Tech/2023/23_01/2023_01_01_programmers_기지국설치.py
+++ My_Algorithm/None_Tech/2023/23_01/2023_01_01_programmers_기지국설치.py
@@ -2,25 +2,9 @@
 
 def solution(n, stations, w):
     result = 0
-    total_range = 2 * w + 1
-    start = 1
-    for st in stations:
-        width = st - w
-        if width <= start:
-            continue
-        cnt = 1
-        cur_range = total_range
-        while cur_range < width - 1:
-            cur_range += cur_range
-            cnt += 1
-        result += cnt
-    last_range = n - stations[-1] - w
-    cnt = 1
-    while total_range < last_range - 1:
-        total_range += total_range
-        cnt += 1
+    station_range = 2 * w + 1
     
-    return result + cnt
+    return result
 
 print(solution(11, [4, 11], 1))
 print(solution(16, [9], 2))
