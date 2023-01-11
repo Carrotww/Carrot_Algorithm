734d4feb1d72db1c184ef233a53aa5a3ad71f3ff
Carrotww <rjgjfl90@naver.com>
2023-01-11 23:56:14 +0900

    solve

--- My_Algorithm/None_Tech/2023/23_01/2023_01_11_leetcode5_Longest Palindromic Substring.py
+++ My_Algorithm/None_Tech/2023/23_01/2023_01_11_leetcode5_Longest Palindromic Substring.py
@@ -1,3 +1,4 @@
+"""
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         # palindromic 인지 확인하는 함수
@@ -22,6 +23,25 @@ class Solution:
                     result = max(result, s[i:x], key=lambda x: len(x))
                     continue
         
+        return result
+
+temp = Solution()
+"""
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def expand(left: int, right: int) -> str:
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left+1:right]
+        
+        if len(s) == 1 or s == s[::-1]:
+            return s
+        
+        result = ''
+        for i in range(len(s) - 1):
+            result = max(result, expand(i, i+1), expand(i, i+2), key=len)
         return result
 
 temp = Solution()
