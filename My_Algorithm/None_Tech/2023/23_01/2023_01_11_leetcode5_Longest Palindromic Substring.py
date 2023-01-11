20284bf4dde7743ed33b055aa6e9d35509a0ebed
Carrotww <rjgjfl90@naver.com>
2023-01-12 00:23:19 +0900

    commit

--- My_Algorithm/None_Tech/2023/23_01/2023_01_11_leetcode5_Longest Palindromic Substring.py
+++ My_Algorithm/None_Tech/2023/23_01/2023_01_11_leetcode5_Longest Palindromic Substring.py
@@ -30,7 +30,7 @@ temp = Solution()
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def expand(left: int, right: int) -> str:
+        def expand(left, right):
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 left -= 1
                 right += 1
@@ -41,7 +41,8 @@ class Solution:
         
         result = ''
         for i in range(len(s) - 1):
-            result = max(result, expand(i, i+1), expand(i, i+2), key=len)
+            result = max(result, expand(i, i+1), expand(i, i+2), key=lambda x:len(x))
+
         return result
 
 temp = Solution()
