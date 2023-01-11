f50ed1106ef54a462b70f4491485318f6b43a13c
Carrotww <rjgjfl90@naver.com>
2023-01-12 01:21:03 +0900

    commit

--- My_Algorithm/None_Tech/2023/23_01/2023_01_11_leetcode1_Two sum.py
+++ My_Algorithm/None_Tech/2023/23_01/2023_01_11_leetcode1_Two sum.py
@@ -10,7 +10,7 @@ class Solution:
                     return [i, x]
 
 # in을 이용한 탐색
-class Solutiuon:
+class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
             temp = target - nums[i]
@@ -19,3 +19,21 @@ class Solutiuon:
                 return [i, nums[i+1:].index(temp) + i + 1]
 
 # 첫 번째 수를 뺀 결과 키 조회
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_dict = {}
+        for i, num in enumerate(nums):
+            nums_dict[num] = i
+        
+        for i, num in enumerate(nums):
+            if target - num in nums_dict and i != nums_dict[target - num]:
+                return [i, nums_dict[target - num]]
+
+# 위 풀이 개선
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_dict = {}
+        for i, num in enumerate(nums):
+            if target - num in nums_dict:
+                return [i, nums_dict[target-num]]
+            nums_dict[num] = i
