3e0fca22e91dc595edf45e02d5aa41219186cf63
Carrot_hyeong <rjgjfl90@naver.com>
2023-01-18 00:58:00 +0900

    commit

--- My_Algorithm/None_Tech/2023/23_01/2023_01_17_leetcode238_Product of Array.py
+++ My_Algorithm/None_Tech/2023/23_01/2023_01_17_leetcode238_Product of Array.py
@@ -3,4 +3,14 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        return
+        result = []
+        temp = 1
+        for i in range(len(nums)):
+            result.append(temp)
+            temp *= nums[i]
+        temp = 1
+        for i in range(len(nums)-1, -1, -1):
+            result[i] = result[i] * temp
+            temp *= nums[i]
+        
+        return result
