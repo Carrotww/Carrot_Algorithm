18669903ba033b8d30300161d46e06473ce140fb
Carrot_hyeong <rjgjfl90@naver.com>
2023-01-23 21:30:43 +0900

    leetcode 316

--- My_Algorithm/None_Tech/2023/23_01/2023_01_22_leetcode316_Remove Duplicate.py
+++ My_Algorithm/None_Tech/2023/23_01/2023_01_22_leetcode316_Remove Duplicate.py
@@ -2,6 +2,18 @@
 
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
+        import collections
+        counter = collections.Counter(s)
+        stack = []
         
+        for char in s:
+            counter[char] -= 1
+            if char in stack:
+                continue
+            
+            while stack and char < stack[-1] and counter[stack[-1]] >= 1:
+                stack.pop()
+            
+            stack.append(char)
         
-        return
+        return ''.join(stack)
