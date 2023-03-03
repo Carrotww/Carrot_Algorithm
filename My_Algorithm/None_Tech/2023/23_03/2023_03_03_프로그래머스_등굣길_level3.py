75d3a3779d66b9adb552b38403a8c71c19063365
Carrot_hyeong <rjgjfl90@naver.com>
2023-03-03 17:11:01 +0900

    2023 03 03

---
+++ My_Algorithm/None_Tech/2023/23_03/2023_03_03_프로그래머스_등굣길_level3.py
@@ -0,0 +1,29 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42898
+
+def solution(m, n, puddles):
+    from collections import deque
+    dp = [[10001 for _ in range(n)] for _ in range(m)]
+    path = dp[:]
+    dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
+    for pu in puddles:
+        x, y = pu
+        path[x-1][y-1] = -1
+    
+    fast_cnt = 10001
+    result = 0
+    queue = deque()
+    queue.append([0, 0, 0])
+    while queue:
+        cur_x, cur_y, cnt = queue.popleft()
+        for i in range(4):
+            n_x, n_y = cur_x + dx[i], cur_y + dy[i]
+            if (n_x >= 0 and n_x < m) and (n_y >= 0 and n_y < n) and path[n_x][n_y] != -1:
+                if n_x == m - 1 and n_y == n - 1 and (cnt + 1 <= fast_cnt):
+                    fast_cnt = cnt + 1
+                    result += 1
+                    break
+                if cnt + 1 <= dp[n_x][n_y]:
+                    dp[n_x][n_y] = cnt + 1
+                    queue.append([n_x, n_y, cnt + 1])
+    
+    return result % 1000000007
