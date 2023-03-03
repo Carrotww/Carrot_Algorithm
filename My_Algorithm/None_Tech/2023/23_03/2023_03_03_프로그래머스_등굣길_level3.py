7d8f4296ab0b87a62c9f1a3ecfa320a72620064c
Carrot_hyeong <rjgjfl90@naver.com>
2023-03-03 18:01:03 +0900

    프로그래머스 dp 등굣길 풀이

--- My_Algorithm/None_Tech/2023/23_03/2023_03_03_프로그래머스_등굣길_level3.py
+++ My_Algorithm/None_Tech/2023/23_03/2023_03_03_프로그래머스_등굣길_level3.py
@@ -28,3 +28,18 @@ def solution(m, n, puddles):
                     queue.append([n_x, n_y, cnt + 1])
     
     return result % 1000000007
+
+# 정답 풀이
+def solution(m, n, puddles):
+    dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if i == 1 and j == 1:
+                continue
+            if [j, i] in puddles:
+                dp[i][j] = 0
+            else:
+                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000007
+    
+    return dp[i][j]
