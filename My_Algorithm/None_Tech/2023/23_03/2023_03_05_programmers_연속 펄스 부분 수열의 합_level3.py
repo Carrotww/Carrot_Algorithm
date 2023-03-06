f0da367546953ac7e37e780ca0c40e9503dcc40c
Carrot_hyeong <rjgjfl90@naver.com>
2023-03-06 22:38:53 +0900

    연속 펄스 부분 수열의 합 풀이

--- My_Algorithm/None_Tech/2023/23_03/2023_03_05_programmers_연속 펄스 부분 수열의 합_level3.py
+++ My_Algorithm/None_Tech/2023/23_03/2023_03_05_programmers_연속 펄스 부분 수열의 합_level3.py
@@ -59,3 +59,18 @@ def solution(sequence):
                 result = max(result, total)
         
     return result
+
+
+# 두 번째 풀이
+def solution(sequence):
+    dp1 = [0] * len(sequence)
+    dp2 = dp1[::]
+    
+    dp1[0] = sequence[0]
+    dp2[0] = -sequence[0]
+    
+    for i in range(1, len(sequence)):
+        dp1[i] = max(dp2[i-1]+sequence[i], sequence[i])
+        dp2[i] = max(dp1[i-1]-sequence[i], -sequence[i])
+    
+    return max(max(dp1), max(dp2))
