21282f1c137ae7ebc0f9954198a2cb85e15b37c3
Carrot_hyeong <rjgjfl90@naver.com>
2023-03-07 23:35:12 +0900

    palindrome hard 첫 번째 풀이

---
+++ My_Algorithm/None_Tech/2023/23_03/2023_03_07_leetcode_Palindrome Pairs_hard.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/palindrome-pairs/
+
+from typing import List
+
+class Solution:
+    def palindromePairs(self, words: List[str]) -> List[List[int]]:
+        def checkpalin(char):
+            left, right = 0, len(char)-1
+            if len(char) % 2 == 0:
+                while right != (len(char) // 2)-1:
+                    if char[left] != char[right]:
+                        return False
+                    left += 1
+                    right -= 1
+            else:
+                while right != (len(char) // 2):
+                    if char[left] != char[right]:
+                        return False
+                    left += 1
+                    right -= 1
+            return True
+        
+        result = []
+        for i in range(len(words)):
+            for j in range(len(words)):
+                if i == j:
+                    continue
+                if checkpalin(words[i] + words[j]):
+                    result.append([i, j])
+        
+        return result
