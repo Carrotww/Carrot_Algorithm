c7ed0e9d3d1a57d68c641eff81f72c9c24b0544c
Carrot_hyeong <rjgjfl90@naver.com>
2023-03-21 20:20:25 +0900

    leetcode 56 solve2

--- My_Algorithm/None_Tech/2023/23_03/2023_03_21_Leetcode56_MergeIntervals.py
+++ My_Algorithm/None_Tech/2023/23_03/2023_03_21_Leetcode56_MergeIntervals.py
@@ -22,3 +22,23 @@ class Solution:
                 pre_val = intervals[i][1]
 
         return result
+
+
+# 조금 직관적인 풀이
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        result = []
+        intervals.sort(key=lambda x:x[0])
+
+        result.append(intervals[0])
+        pre_val = intervals[0][1]
+
+        for i in range(1, len(intervals)):
+            if pre_val >= intervals[i][0]:
+                result[-1][1] = max(pre_val, intervals[i][1])
+                pre_val = result[-1][1]
+            else:
+                result.append(intervals[i])
+                pre_val = intervals[i][1]
+
+        return result
