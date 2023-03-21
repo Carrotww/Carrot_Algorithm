6fa015a484ffa8e72e6f43fb8b8441d3254f0a8a
Carrot_hyeong <rjgjfl90@naver.com>
2023-03-21 20:16:53 +0900

    leetcode 56 solve

--- My_Algorithm/None_Tech/2023/23_03/2023_03_21_Leetcode56_MergeIntervals.py
+++ My_Algorithm/None_Tech/2023/23_03/2023_03_21_Leetcode56_MergeIntervals.py
@@ -4,5 +4,21 @@ from typing import List
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        result = []
+        intervals.sort(key=lambda x:x[0])
 
-        return
+        result.append(intervals[0])
+        pre_val = intervals[0][1]
+
+        for i in range(1, len(intervals)):
+            if pre_val >= intervals[i][0]:
+                if pre_val >= intervals[i][1]:
+                    pass
+                else:
+                    result[-1][1] = intervals[i][1]
+                    pre_val = intervals[i][1]
+            else:
+                result.append(intervals[i])
+                pre_val = intervals[i][1]
+
+        return result
