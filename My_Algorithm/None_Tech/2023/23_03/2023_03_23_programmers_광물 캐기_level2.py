f8bc8cffe309d45f8b2ff83822834cc9c7f35215
Carrot_hyeong <rjgjfl90@naver.com>
2023-03-31 02:10:32 +0900

    광물캐기 품

--- My_Algorithm/None_Tech/2023/23_03/2023_03_23_programmers_광물 캐기_level2.py
+++ My_Algorithm/None_Tech/2023/23_03/2023_03_23_programmers_광물 캐기_level2.py
@@ -2,14 +2,54 @@
 
 # 푸는 중
 def solution(picks, minerals):
-    cur_pick = [-1, 0]
+    result = 0
     
-    for min in minerals:
-        if cur_pick[1] == 0:
-            if sum(picks) == 0:
-                break
-            if min == 'diamond':
-                
-            
-            
-        if min == 'diamond':
+    minerals = minerals[:sum(picks)*5]
+    slice_min = []
+    
+    def count_mineral(min_list):
+        result = 0
+        check_dict = {"diamond" : 15, "iron" : 5, "stone" : 1}
+        for val in min_list:
+            result += check_dict[val]
+        
+        return result
+    
+    cnt = 0
+    tmp = []
+    for mine in minerals:
+        tmp.append(mine)
+        cnt += 1
+        if cnt == 5:
+            weight = count_mineral(tmp)
+            slice_min.append([weight, tmp])
+            tmp = []
+            cnt = 0
+    
+    if tmp:
+        weight = count_mineral(tmp)
+        slice_min.append([weight, tmp])
+    
+    slice_min.sort(key=lambda x:x[0])
+    
+    iron_graph = {"diamond" : 5, "iron" : 1, "stone" : 1}
+    stone_graph = {"diamond" : 25, "iron" : 5, "stone" : 1}
+    
+    for i in range(3):
+        for x in range(picks[i]):
+            if slice_min:
+                if i == 0:
+                    mineral_list = slice_min.pop()[1]
+                    result += len(mineral_list)
+                elif i == 1:
+                    mineral_list = slice_min.pop()[1]
+                    for mineral in mineral_list:
+                        result += iron_graph[mineral]
+                else:
+                    mineral_list = slice_min.pop()[1]
+                    for mineral in mineral_list:
+                        result += stone_graph[mineral]
+            else:
+                return result
+    
+    return result
