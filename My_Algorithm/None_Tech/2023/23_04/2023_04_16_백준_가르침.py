059c1b27294bfeef6133f7285e1c12bffebb5bb8
Carrot_hyeong <rjgjfl90@naver.com>
2023-04-20 00:08:52 +0900

    백준 가르침 추가 풀이 test

--- My_Algorithm/None_Tech/2023/23_04/2023_04_16_백준_가르침.py
+++ My_Algorithm/None_Tech/2023/23_04/2023_04_16_백준_가르침.py
@@ -1,94 +1,135 @@
 # https://www.acmicpc.net/problem/1062
 
-def solve():
-    import sys
-    N, K = map(int, sys.stdin.readline().split())
-    result = 0
+# def solve():
+#     import sys
+#     N, K = map(int, sys.stdin.readline().split())
+#     result = 0
     
-    alpa = {'a', 'n', 't', 'i', 'c'}
+#     alpa = {'a', 'n', 't', 'i', 'c'}
 
-    total_need_alpa = {}
-    alpa_list = []
+#     total_need_alpa = {}
+#     alpa_list = []
 
-    for i in range(N):
-        split_alpa = sys.stdin.readline().strip()[4:-4]
-        alpa_list.append(split_alpa)
-        need_alpa = set()
-        for cur_alpa in split_alpa:
-            if cur_alpa not in alpa:
-                need_alpa.add(cur_alpa)
-        if not need_alpa:
-            continue
-        need_alpa = ''.join(list(need_alpa))
-        if need_alpa in total_need_alpa:
-            total_need_alpa[need_alpa] += 1
-        else:
-            total_need_alpa[need_alpa] = 1
+#     for i in range(N):
+#         split_alpa = sys.stdin.readline().strip()[4:-4]
+#         alpa_list.append(split_alpa)
+#         need_alpa = set()
+#         for cur_alpa in split_alpa:
+#             if cur_alpa not in alpa:
+#                 need_alpa.add(cur_alpa)
+#         if not need_alpa:
+#             continue
+#         need_alpa = ''.join(list(need_alpa))
+#         if need_alpa in total_need_alpa:
+#             total_need_alpa[need_alpa] += 1
+#         else:
+#             total_need_alpa[need_alpa] = 1
 
-    if K < 5:
-        print(result)
-        return
+#     if K < 5:
+#         print(result)
+#         return
     
-    sorted_total_need_alpa = sorted(total_need_alpa.items(), key=lambda x: (x[0], x[1]), reverse=True)
-    # print(sorted_total_need_alpa)
+#     sorted_total_need_alpa = sorted(total_need_alpa.items(), key=lambda x: (x[0], x[1]), reverse=True)
+#     # print(sorted_total_need_alpa)
 
-    cnt = 0
-    for val, idx in sorted_total_need_alpa:
-        for v in val:
-            if cnt == K - 5:
-                break
-            if v not in alpa:
-                alpa.add(v)
-                cnt += 1
+#     cnt = 0
+#     for val, idx in sorted_total_need_alpa:
+#         for v in val:
+#             if cnt == K - 5:
+#                 break
+#             if v not in alpa:
+#                 alpa.add(v)
+#                 cnt += 1
     
-    for cur_word in alpa_list:
-        is_add = True
-        for cur_alpa in cur_word:
-            if cur_alpa not in alpa:
-                is_add = False
-        if is_add:
-            result += 1
+#     for cur_word in alpa_list:
+#         is_add = True
+#         for cur_alpa in cur_word:
+#             if cur_alpa not in alpa:
+#                 is_add = False
+#         if is_add:
+#             result += 1
     
-    print(result)
-    return
+#     print(result)
+#     return
 
-def solve2():
-    from itertools import combinations
-    import sys
-    N, K = map(int, sys.stdin.readline().split())
-    word_list = [sys.stdin.readline().strip()[4:-4] for _ in range(N)]
-    base_char = {'a', 'n', 't', 'i', 'c'}
+# def solve2():
+#     from itertools import combinations
+#     import sys
+#     N, K = map(int, sys.stdin.readline().split())
+#     word_list = [sys.stdin.readline().strip()[4:-4] for _ in range(N)]
+#     base_char = {'a', 'n', 't', 'i', 'c'}
 
-    visited = set([])
+#     visited = set([])
 
-    for word in word_list:
-        for char in word:
-            if char not in base_char:
-                visited.add(char)
+#     for word in word_list:
+#         for char in word:
+#             if char not in base_char:
+#                 visited.add(char)
     
-    visited = list(visited)
-    result = 0
+#     visited = list(visited)
+#     result = 0
     
-    if K < 5:
-        print(result)
-    else:
-        K -= 5
-        if K > len(visited):
-            K = len(visited)
+#     if K < 5:
+#         print(result)
+#     else:
+#         K -= 5
+#         if K > len(visited):
+#             K = len(visited)
         
-        for com in combinations(visited, K):
-            cnt = 0
+#         for com in combinations(visited, K):
+#             cnt = 0
+#             for word in word_list:
+#                 for w in word:
+#                     if w not in base_char and w not in com:
+#                         break
+#                 else:
+#                     cnt += 1
+#             result = max(result, cnt)
+#         print(result)
+    
+def solve3():
+    import sys
+    N, K = map(int, sys.stdin.readline().split())
+    # word_list = [set(sys.stdin.readline().strip()[4:-4]) for _ in range(N)]
+    word_list = [sys.stdin.readline().strip() for _ in range(N)]
+    alph = [0] * 26
+    result = 0
+
+    def dfs(idx, cnt):
+        global result
+        
+        if K - 5 == cnt:
+            read_cnt = 0
             for word in word_list:
+                is_read = True
                 for w in word:
-                    if w not in base_char and w not in com:
+                    if alph[ord(w) - ord('a')] == 0:
+                        is_read = False
                         break
-                else:
-                    cnt += 1
-            result = max(result, cnt)
+                if is_read:
+                    read_cnt += 1
+            result = max(result, read_cnt)
+        
+        for i in range(idx, 26):
+            if alph[i] == 0:
+                alph[i] = 1
+                dfs(i, cnt+1)
+                alph[i] = 0
+
+    if K < 5:
+        print(0)
+    else:
+        for char in ('a', 'c', 'i', 'n', 't'):
+            alph[ord(char) - ord('a')] = 1
+        
+        dfs(0, 0)
         print(result)
 
 if __name__ == "__main__":
-    solve2()
+    # solve()
+    # solve2()
+    solve3()
+
 # 6 6
 # antabcdefgtica
 # antabcdefgtica
@@ -105,3 +146,45 @@ antazzztica
 antazzztica
 antazzztica
 '''
+
+def solve3():
+    N, K = 9, 8
+    word_list = [set(w[4:-4]) for w in ["antabtica", "antaxtica", "antadtica", "antaetica", "antaftica", "antagtica", "antahtica", "antajtica", "antaktica"]]
+    alph = [0] * 26
+
+    def dfs(idx, cnt):
+        nonlocal result  # Use 'nonlocal' instead of 'global'
+        result = 0
+        
+        if K - 5 == cnt:
+            read_cnt = 0
+            for word in word_list:
+                is_read = True
+                for w in word:
+                    if alph[ord(w) - ord('a')] == 0:
+                        is_read = False
+                        break
+                if is_read:
+                    read_cnt += 1
+            result = max(result, read_cnt)
+            return result
+        
+        for i in range(idx, 26):
+            if alph[i] == 0:
+                alph[i] = 1
+                result = max(result, dfs(i, cnt+1))
+                alph[i] = 0
+                
+        return result
+
+    if K < 5:
+        print(0)
+    else:
+        for char in ('a', 'c', 'i', 'n', 't'):
+            alph[ord(char) - ord('a')] = 1
+    
+        result = dfs(0, 0)
+        print(result)
+
+if __name__ == "__main__":
+    solve3()
