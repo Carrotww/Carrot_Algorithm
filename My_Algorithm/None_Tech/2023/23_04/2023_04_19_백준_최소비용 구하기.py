d63e0f85422ece511a4224d8416324cdab6740c6
Carrot_hyeong <rjgjfl90@naver.com>
2023-04-20 23:30:59 +0900

    백준 최소비용 구하기 다익스트라

--- My_Algorithm/None_Tech/2023/23_04/2023_04_19_백준_최소비용 구하기.py
+++ My_Algorithm/None_Tech/2023/23_04/2023_04_19_백준_최소비용 구하기.py
@@ -2,11 +2,34 @@
 
 def solve():
     import sys
+    import heapq
+    import math
+    from collections import defaultdict
+
     N = int(sys.stdin.readline())
     M = int(sys.stdin.readline())
-    bus_list = []
+    graph = defaultdict(list)
     
     for _ in range(M):
-        bus_list.append(list(map(int, sys.stdin.readline().split())))
+        start, end, weight = map(int, sys.stdin.readline().split())
+        graph[start].append([end, weight])
     
     start_city, end_city = map(int, sys.stdin.readline().split())
+    visited = [math.inf] * (N+1)
+    visited[start_city] = 0
+
+    queue = [(0, start_city)]
+
+    while queue:
+        weight, cur_node = heapq.heappop(queue)
+        if weight > visited[cur_node]:
+            continue
+        for end, n_weight in graph[cur_node]:
+            if visited[end] > n_weight + weight:
+                visited[end] = n_weight + weight
+                heapq.heappush(queue, (n_weight + weight, end))
+    
+    print(visited[end_city])
+
+if __name__ == "__main__":
+    solve()
