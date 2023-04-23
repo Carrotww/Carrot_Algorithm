bb06b2567c0cb239dd7e88eb5c2558b7fa7a6f5c
6youngEH <a42230@naver.com>
2023-04-23 17:29:43 +0900

    백준 요세푸스 풀이

--- My_Algorithm/None_Tech/2023/23_04/2023_04_21_백준_요세푸스.py
+++ My_Algorithm/None_Tech/2023/23_04/2023_04_21_백준_요세푸스.py
@@ -1,1 +1,20 @@
 # https://www.acmicpc.net/problem/1158
+
+def solve():
+    import sys
+    N, K = map(int, sys.stdin.readline().split())
+    num_list = [x for x in range(1, N+1)]
+    result = []
+    idx = 0
+    
+    for i in range(N):
+        idx += (K-1)
+        if idx >= N:
+            idx %= N
+        result.append(str(num_list.pop(idx)))
+        N -= 1
+
+    print("<",", ".join(result)[:],">", sep='')
+
+if __name__ == "__main__":
+    solve()
