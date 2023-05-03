e2ec71a18dcc6531cc4e1c60213fdec714dc901d
Carrot_hyeong <rjgjfl90@naver.com>
2023-05-03 22:52:26 +0900

    백준 숨바꼭질4 풀이

--- My_Algorithm/None_Tech/2023/23_05/2023_05_02_백준_숨바꼭질4.py
+++ My_Algorithm/None_Tech/2023/23_05/2023_05_02_백준_숨바꼭질4.py
@@ -3,15 +3,36 @@
 def solve():
     import sys
     import heapq
-    from collections import deque
     
     N, K = map(int, sys.stdin.readline().split())
     path = [-1] * 100001
+    visited = [-1] * 100001
 
     heap = []
+    result_time = 0
     heapq.heappush(heap, [0, N])
     while heap:
         cur_time, cur_node = heapq.heappop(heap)
-        for i in [cur_node+1, cur_node-1, cur_node*2]:
-            if i < 0 or i > 100000:
-                pass
+        if cur_node == K:
+            result_time = cur_time
+            break
+            
+        for n_node in [cur_node+1, cur_node-1, cur_node*2]:
+            if n_node < 0 or n_node > 100000 or visited[n_node] != -1:
+                continue
+            visited[n_node] = cur_time + 1
+            path[n_node] = cur_node
+            heapq.heappush(heap, [cur_time+1, n_node])
+    
+    print(result_time)
+    
+    result_path = []
+    start, end = N, K
+    result_path.append(end)
+    while start != end:
+        result_path.append(path[end])
+        end = path[end]
+    print(' '.join([str(x) for x in result_path[::-1]]))
+
+if __name__ == "__main__":
+    solve()
