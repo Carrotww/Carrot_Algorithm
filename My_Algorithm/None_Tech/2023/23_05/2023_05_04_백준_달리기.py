0a8cfe9ea7a58661f2ed2e3f262f32ab6395a621
Carrot_hyeong <rjgjfl90@naver.com>
2023-05-05 17:40:16 +0900

    백준 달리기

--- My_Algorithm/None_Tech/2023/23_05/2023_05_04_백준_달리기.py
+++ My_Algorithm/None_Tech/2023/23_05/2023_05_04_백준_달리기.py
@@ -1,6 +1,6 @@
 # https://www.acmicpc.net/problem/16930
 
-def solve():
+def solve2():
     import sys
     from collections import deque
     
@@ -12,18 +12,60 @@ def solve():
     x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
     x1, y1, x2, y2 = x1-1, y1-1, x2-1, y2-1
 
-    path = [[-1] * M for _ in range(N)]
+    path = [[False] * M for _ in range(N)]
     visited = [[-1] * M for _ in range(N)]
+    visited[x1][y1] = 1
     queue = deque()
-    queue.append([x1, y1])
+    queue.append([x1, y1, 0])
     dr, dc = [1, -1, 0, 0], [0, 0, 1, -1]
+    result_cnt = 0
 
     while queue:
-        cur_r, cur_c = queue.popleft()
+        cur_r, cur_c, cnt = queue.popleft()
         if cur_r == x2 and cur_c == y2:
+            result_cnt = cnt
             break
         for i in range(4):
             n_r, n_c = cur_r+dr[i], cur_c+dc[i]
+            if (n_r < 0 or n_r >= N) or (n_c < 0 or n_c >= M) or graph[n_r][n_c] != '.' or visited[n_r][n_c] != -1:
+                continue
+            visited[n_r][n_c] = 1
+            path[n_r][n_c] = [cur_r, cur_c]
+            queue.append([n_r, n_c, cnt+1])
+    
+    if result_cnt == 0:
+        print(-1)
+    else:
+        path_check = [[x2, y2]]
+        while (x1 != x2) or (y1 != y2):
+            t1, t2 = path[x2][y2]
+            path_check.append([t1, t2])
+            x2, y2 = t1, t2
+        
+        cnt = 0
+        k_cnt = 0
+        pre_direct = None
+
+        for i in range(len(path_check) - 1):
+            x1, y1 = path_check[i]
+            x2, y2 = path_check[i+1]
+
+            if x1 == x2:
+                direct = "W"
+            elif y1 == y2:
+                direct = "H"
+            
+            if pre_direct and pre_direct == direct and cnt < K:
+                cnt += 1
+            else:
+                pre_direct = direct
+                cnt = 1
+                k_cnt += 1
+        
+        print(k_cnt)
+
+def solve():
+    pass
 
 if __name__ == "__main__":
     solve()
