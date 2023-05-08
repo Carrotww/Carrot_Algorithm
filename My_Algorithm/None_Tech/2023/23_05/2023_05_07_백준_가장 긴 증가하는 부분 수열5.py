0e2c8da139f5e2f686b4c9e6cef56fa96747e2e5
Carrot_hyeong <rjgjfl90@naver.com>
2023-05-09 01:25:18 +0900

    백준 가장 긴 증가하는 부분수열 5 임시

--- My_Algorithm/None_Tech/2023/23_05/2023_05_07_백준_가장 긴 증가하는 부분 수열5.py
+++ My_Algorithm/None_Tech/2023/23_05/2023_05_07_백준_가장 긴 증가하는 부분 수열5.py
@@ -26,3 +26,50 @@ def solve():
 
 if __name__ == "__main__":
     solve()
+
+def solve():
+    import sys
+    N = int(sys.stdin.readline())
+    array = list(map(int, sys.stdin.readline().split()))
+
+    LIS = [array[0]]
+    positions = [0]
+    parent = [-1] * N
+
+    def findidx(value):
+        left = 0
+        right = len(LIS) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if LIS[mid] < value:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return left
+
+    for i, val in enumerate(array[1:], 1):
+        if LIS[-1] < val:
+            parent[i] = positions[-1]
+            LIS.append(val)
+            positions.append(i)
+        else:
+            idx = findidx(val)
+            LIS[idx] = val
+            positions[idx] = i
+            if idx > 0:
+                parent[i] = positions[idx - 1]
+
+    sequence = []
+    index = positions[-1]
+    while index != -1:
+        sequence.append(array[index])
+        index = parent[index]
+    sequence.reverse()
+
+    print(len(LIS))
+    print(" ".join(map(str, sequence)))
+
+if __name__ == "__main__":
+    solve()
