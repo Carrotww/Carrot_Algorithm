a11a7d648fc38d44d772405393f173d699b96010
Carrot_hyeong <rjgjfl90@naver.com>
2023-05-10 00:16:53 +0900

    백준 최단 경로 풀이

--- My_Algorithm/None_Tech/2023/23_05/2023_05_09_백준_최단경로.py
+++ My_Algorithm/None_Tech/2023/23_05/2023_05_09_백준_최단경로.py
@@ -2,17 +2,35 @@
 
 def solve():
     import sys
+    import heapq
+    from collections import defaultdict
     # V -> node cnt E -> edge cnt
     V, E = map(int, sys.stdin.readline().split())
     # K -> start node
     K = int(sys.stdin.readline())
-    graph = {x:[] for x in range(1, V+1)}
+    graph = defaultdict(list)
+    visited = [float('inf')] * (V + 1)
+    visited[K] = 0
 
     for _ in range(E):
         start, end, w = map(int, sys.stdin.readline().split())
         graph[start].append([end, w])
     
-    print(graph)
+    heap = []
+    heapq.heappush(heap, [0, K])
+
+    while heap:
+        cur_weight, cur_node = heapq.heappop(heap)
+        for n_node, n_weight in graph[cur_node]:
+            if cur_weight + n_weight < visited[n_node]:
+                visited[n_node] = cur_weight + n_weight
+                heapq.heappush(heap, [cur_weight + n_weight, n_node])
+    
+    for i in range(1, len(visited)):
+        if visited[i] == float('inf'):
+            print('INF')
+        else:
+            print(visited[i])
 
 if __name__ == "__main__":
     solve()
