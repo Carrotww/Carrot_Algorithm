542517818fa16dd7af2b48175e0c6b26af3c9b1b
Carrot_hyeong <rjgjfl90@naver.com>
2023-05-11 21:10:18 +0900

    백준 로봇 풀이

--- My_Algorithm/None_Tech/2023/23_05/2023_05_10_백준_로봇--hold.py
+++ My_Algorithm/None_Tech/2023/23_05/2023_05_10_백준_로봇--hold.py
@@ -23,19 +23,16 @@ def solve():
 
     while queue:
         cur_r, cur_c, cur_direct, cnt = queue.popleft()
-        # if cur_r == end_r and cur_c == end_c and cur_direct == end_direct:
-        if (cur_r, cur_c, cur_direct) == (end_r, end_c, end_direct):
+        if cur_r == end_r and cur_c == end_c and cur_direct == end_direct:
             print(cnt)
             break
 
         for k in range(1, 4):
             n_d = cur_direct
             n_r, n_c = dr[n_d]*k + cur_r, dc[n_d]*k + cur_c
-            # if (n_r < 0 or n_r >= row) or (n_c < 0 or n_c >= col) or graph[n_r][n_c] == 1 or visited[n_r][n_c][cur_direct] == 1:
-            #     continue
-            if not (0 <= n_r < row and 0 <= n_c < 0 ) or graph[n_r][n_c]:
+            if (n_r < 0 or n_r >= row) or (n_c < 0 or n_c >= col) or graph[n_r][n_c] == 1:
                 break
-            if visited[n_r][n_c][n_d] == 1:
+            if visited[n_r][n_c][cur_direct] == 1:
                 continue
             queue.append([n_r, n_c, n_d, cnt+1])
             visited[n_r][n_c][n_d] = 1
