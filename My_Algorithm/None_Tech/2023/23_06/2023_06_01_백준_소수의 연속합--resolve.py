6aef005575a5dd0c464e9093db91163d226be3b6
Carrot_hyeong <rjgjfl90@naver.com>
2023-06-01 23:43:25 +0900

    백준 소수 연속 합 투포인터 풀이 추가

--- My_Algorithm/None_Tech/2023/23_06/2023_06_01_백준_소수의 연속합--resolve.py
+++ My_Algorithm/None_Tech/2023/23_06/2023_06_01_백준_소수의 연속합--resolve.py
@@ -3,32 +3,65 @@
 def prime_list(n):
     is_prime = [True] * (n + 1)
     is_prime[0], is_prime[1] = False, False
-    
+
     for i in range(2, int(n ** 0.5) + 1):
         if is_prime[i]:
             for j in range(i*i, n+1, i):
                 is_prime[j] = False
-    
+
     return [x for x in range(n+1) if is_prime[x]]
 
-def solve():
+
+def solve1():
     import sys
     num = int(sys.stdin.readline())
     primes = prime_list(num)
     sum_list = [0] * (len(primes) + 1)
     cnt = 0
-    
+
     for i in range(1, len(primes) + 1):
         sum_list[i] = sum_list[i-1] + primes[i-1]
-    
+
     for i in range(len(primes)):
         for j in range(i+1, len(primes)+1):
             if sum_list[j] - sum_list[i] == num:
                 cnt += 1
             elif sum_list[j] - sum_list[i] > num:
                 break
-    
+
     print(cnt)
+
+
+def solve():
+    import sys
+    num = int(sys.stdin.readline())
+    if num == 1:
+        print(0)
+        return
+    primes = prime_list(num)
+
+    total = primes[0]
+    start, end = 0, 0
+    result = 0
+    while end <= len(primes):
+        if total == num:
+            result += 1
+            end += 1
+            if end < len(primes):
+                total += primes[end]
+            else:
+                break
+        elif total < num:
+            end += 1
+            if end < len(primes):
+                total += primes[end]
+            else:
+                break
+        else:
+            total -= primes[start]
+            start += 1
+
+    print(result)
 
 if __name__ == "__main__":
     solve()
