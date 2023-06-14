a7102dc60b8fb885131068a731088459b5484b93
Carrot_hyeong <rjgjfl90@naver.com>
2023-06-14 20:21:26 +0900

    백준 cowLineup 품

--- My_Algorithm/None_Tech/2023/23_06/2023_06_11_Cow Lineup--푸는중.py
+++ My_Algorithm/None_Tech/2023/23_06/2023_06_11_Cow Lineup--푸는중.py
@@ -5,30 +5,36 @@ def solve():
     from collections import defaultdict
     N = int(sys.stdin.readline())
     cow_list = []
+    breed_dict = defaultdict(int)
     breed_set = set()
 
     for _ in range(N):
         x, breed = map(int, sys.stdin.readline().split())
         breed_set.add(breed)
-        cow_list.append((x, breed))
+        cow_list.append([x, breed])
+
+    # [15, 1] [20, 1] [22, 3] [25, 7] [26, 1]
+    total = len(breed_set)
+    breed_set = set()
     cow_list.sort()
-    total_breed = len(breed_set)
+    start, end = 0, 0
     result = float('inf')
 
-    for i in range(len(cow_list)):
-        check_set = set()
-        cnt = 0
-        idx = i
-        while idx < N:
-            if cow_list[idx][1] not in check_set:
-                check_set.add(cow_list[idx][1])
-                cnt += 1
-            if cnt == total_breed:
-                result = min(result, cow_list[idx][0] - cow_list[i][0])
-                break
-            idx += 1
-    
+    while end < N:
+        cur_x, cur_breed = cow_list[end]
+        breed_set.add(cur_breed)
+        breed_dict[cur_breed] += 1
+
+        while len(breed_set) == total:
+            result = min(result, cur_x - cow_list[start][0])
+            breed_dict[cow_list[start][1]] -= 1
+            if breed_dict[cow_list[start][1]] == 0:
+                breed_set.remove(cow_list[start][1])
+            start += 1
+        end += 1
+
     print(result)
+
 
 if __name__ == "__main__":
     solve()
