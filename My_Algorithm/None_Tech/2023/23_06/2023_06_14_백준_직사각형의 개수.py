fa0826494ccbc1a892689b5523ea85d1e7150efe
Carrot_hyeong <rjgjfl90@naver.com>
2023-06-15 00:39:43 +0900

    백준 직사각형의 개수

--- My_Algorithm/None_Tech/2023/23_06/2023_06_14_백준_직사각형의 개수.py
+++ My_Algorithm/None_Tech/2023/23_06/2023_06_14_백준_직사각형의 개수.py
@@ -39,6 +39,38 @@ def solve():
 
     print(result)
 
+def solve1():
+    import sys
+    from itertools import combinations
+    from collections import Counter
+    N = int(sys.stdin.readline())
+
+    dot_list = []
+    for _ in range(N):
+        x, y = map(int, sys.stdin.readline().split())
+        dot_list.append([x, y])
+
+    row_dict = dict()
+    for x, y in dot_list:
+        if y not in row_dict:
+            row_dict[y] = [x]
+        else:
+            row_dict[y].append(x)
+
+    couter_list = []
+    for val in row_dict.values():
+        temp = list(map(lambda x: (x[0], x[1]) if x[0] < x[1] else (
+            x[1], x[0]), list(combinations(val, 2))))
+        couter_list += temp
+
+    count_square_dict = Counter(couter_list)
+
+    result = 0
+    for val in count_square_dict.values():
+        if val >= 2:
+            result += (val * (val - 1)) // 2
+
+    print(result)
 
 if __name__ == "__main__":
-    solve()
+    solve1()
