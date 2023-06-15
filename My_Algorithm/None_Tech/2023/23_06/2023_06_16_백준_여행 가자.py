dc5f3fac5caf1f55fc7af3a98a2f55f29c45b3ca
Carrot_hyeong <rjgjfl90@naver.com>
2023-06-16 03:25:58 +0900

    백준 여행가자

--- My_Algorithm/None_Tech/2023/23_06/2023_06_16_백준_여행 가자.py
+++ My_Algorithm/None_Tech/2023/23_06/2023_06_16_백준_여행 가자.py
@@ -42,6 +42,110 @@ def solve():
     else:
         print('NO')
 
+def solve1():
+    import sys
+
+    n = int(sys.stdin.readline())
+    m = int(sys.stdin.readline())
+    parent = dict()
+    travel_path = dict()
+
+    for i in range(1, n+1):
+        parent[i] = i
+        travel_path[i] = [i]
+
+    def find_parent(parent, x):
+        if parent[x] != x:
+            parent[x] = find_parent(parent, parent[x])
+        return parent[x]
+    
+    def union(parent, travel_path, a, b):
+        a = find_parent(parent, a)
+        b = find_parent(parent, b)
+
+        # if a < b:
+        #     parent[b] = a
+        #     travel_path[a] += travel_path[b]
+        # else:
+        #     parent[a] = b
+        #     travel_path[b] += travel_path[a]
+        if a != b:
+            parent[b] = a
+            travel_path[a] += travel_path[b]
+
+    for i in range(1, n+1):
+        temp = list(map(int, sys.stdin.readline().split()))
+        city = 1
+        for te in temp:
+            if te == 1 or i == city:
+                union(parent, travel_path, i, city)
+            city += 1
+    
+    route = list(map(int, sys.stdin.readline().split()))
+    check = set(travel_path[find_parent(parent, route[0])])
+
+    is_yes = True
+    for re in route:
+        if re not in check:
+            is_yes = False
+            break
+    
+    if is_yes:
+        print('YES')
+    else:
+        print('NO')
+
+def solve2():
+    import sys
+
+    n = int(sys.stdin.readline())
+    m = int(sys.stdin.readline())
+    parent = dict()
+    travel_path = dict()
+
+    for i in range(1, n+1):
+        parent[i] = i
+        travel_path[i] = {i}
+
+    def find_parent(parent, x):
+        if parent[x] != x:
+            parent[x] = find_parent(parent, parent[x])
+        return parent[x]
+    
+    def union(parent, travel_path, a, b):
+        a = find_parent(parent, a)
+        b = find_parent(parent, b)
+
+        if a < b:
+            parent[b] = a
+            for t in travel_path[b]:
+                travel_path[a].add(t)
+        else:
+            parent[a] = b
+            for t in travel_path[a]:
+                travel_path[b].add(t)
+
+    for i in range(1, n+1):
+        temp = list(map(int, sys.stdin.readline().split()))
+        city = 1
+        for te in temp:
+            if te == 1 or i == city:
+                union(parent, travel_path, i, city)
+            city += 1
+    
+    route = list(map(int, sys.stdin.readline().split()))
+    check = travel_path[find_parent(parent, route[0])]
+
+    is_yes = True
+    for re in route:
+        if re not in check:
+            is_yes = False
+            break
+    
+    if is_yes:
+        print('YES')
+    else:
+        print('NO')
 
 if __name__ == "__main__":
-    solve()
+    solve2()
