9e957bb319fb2ff4606e1025b53565cf20e1ad1c
Carrot_hyeong <rjgjfl90@naver.com>
2023-06-19 01:39:53 +0900

    풍선 터뜨리기 주식 웜홀 타임머신

---
+++ My_Algorithm/None_Tech/2023/23_06/2023_06_19_백준_웜홀-푸는중.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/1865
+
+def solve():
+    import sys
+    N, M, W = map(int, sys.stdin.readline().split())
+    
+
+if __name__ == "__main__":
+    import sys
+    n = int(sys.stdin.readline())
+    for _ in range(n):
+        solve()
