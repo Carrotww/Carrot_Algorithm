0f124d17d7e475825d87d5b40efd00974cc21384
Carrot_hyeong <rjgjfl90@naver.com>
2023-07-12 11:39:38 +0900

    백준 병사 배치하기

---
+++ My_Algorithm/None_Tech/2023/23_07/2023_07_12_백준_병사 배치하기.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/18353
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.readline
+    n = int(input())
+
+    sol = list(map(int, input().split()))
+
+    dp = [1] * n
+    for i in range(n):
+        for j in range(i):
+            if sol[i] < sol[j]:
+                dp[i] = max(dp[i], dp[j] + 1)
+    print(n - max(dp))
