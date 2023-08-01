715d992fc6b9316c958e6924497696fbabe1d722
Carrot_hyeong <rjgjfl90@naver.com>
2023-08-01 12:31:17 +0900

    백준 토마토 재 풀이

--- 2023/23_08/2023_08_01_백준_토마토.py
+++ 2023/23_08/2023_08_01_백준_토마토.py
@@ -3,31 +3,21 @@
 def bfs():
     from collections import deque
     queue = deque(start_list)
-    dr, dc = [1, 0, -1, 0], [0, 1, 0, -1]
+    dr, dc, dh = [1, 0, -1, 0, 0, 0], [0, 1, 0, -1, 0, 0], [0, 0, 0, 0, 1, -1] 
     global need_change_tomato
     global max_day
 
     while queue:
         cur_h, cur_r, cur_c, cur_day = queue.popleft()
-        for direct in range(4):
-            n_h, n_r, n_c = cur_h, dr[direct]+cur_r, dc[direct]+cur_c
-            if (n_r < 0 or n_r >= r) or (n_c < 0 or n_c >= c):
+        for direct in range(6):
+            n_h, n_r, n_c = dh[direct]+cur_h, dr[direct]+cur_r, dc[direct]+cur_c
+            if (n_r < 0 or n_r >= r) or (n_c < 0 or n_c >= c) or (n_h < 0 or n_h >= h):
                 continue
             if graph[n_h][n_r][n_c] == 0:
                 graph[n_h][n_r][n_c] = 1
                 queue.append((n_h, n_r, n_c, cur_day + 1))
                 need_change_tomato -= 1
                 max_day = max(cur_day + 1, max_day)
-        for i in (-1, 1):
-            n_h, n_r, n_c = cur_h+i, cur_r, cur_c
-            if (n_h < 0 or n_h >= h):
-                continue
-            if graph[n_h][n_r][n_c] == 0:
-                graph[n_h][n_r][n_c] = 1
-                queue.append((n_h, n_r, n_c, cur_day + 1))
-                need_change_tomato -= 1
-                max_day = max(cur_day + 1, max_day)
-
 
 if __name__ == "__main__":
     import sys
