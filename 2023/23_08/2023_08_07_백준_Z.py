50b53a4c4cdb5a6db41dc924cbeb5f618e30ed38
Carrot_hyeong <rjgjfl90@naver.com>
2023-08-07 13:55:03 +0900

    백준 Z 재귀 풀이

--- 2023/23_08/2023_08_07_백준_Z.py
+++ 2023/23_08/2023_08_07_백준_Z.py
@@ -1,5 +1,6 @@
 # # https://www.acmicpc.net/problem/1074
 
+# while 문
 if __name__ == "__main__":
     import sys
     input = sys.stdin.readline
@@ -18,4 +19,28 @@ if __name__ == "__main__":
 
         n -= 1
     
+    print(result)
+
+# 재귀 풀이
+def recursion(n, r, c, total_size):
+    if n == 0:
+        return 0
+    mid_size = total_size // 2
+    
+    temp = 0
+    if r >= mid_size:
+        temp += (mid_size ** 2) * 2
+        r -= mid_size
+    if c >= mid_size:
+        temp += (mid_size ** 2)
+        c -= mid_size
+    
+    return temp + recursion(n-1, r, c, mid_size)
+    
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.readline
+    n, r, c = map(int, input().split())
+    total_size = 2 ** n
+    result = recursion(n, r, c, total_size)
     print(result)
