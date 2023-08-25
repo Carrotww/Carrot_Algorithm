2a5da2d7da16dc3d7abc0bde9d6c0cd6e16a69c8
Carrot_hyeong <rjgjfl90@naver.com>
2023-08-25 17:27:56 +0900

    다시 풀어봄

--- 2023/23_08/2023_08_24_백준_불.py
+++ 2023/23_08/2023_08_24_백준_불.py
@@ -1,5 +1,73 @@
 # https://www.acmicpc.net/problem/4179
 
+# 두 번째 풀이 2708ms
+def j_bfs(cur_list):
+    queue = deque(cur_list)
+    result = 0
+    while queue:
+        cur_r, cur_c, time = queue.popleft()
+        if cur_r == 0 or cur_r == R-1 or cur_c == 0 or cur_c == C-1:
+            result = time + 1
+            break
+        for direct in range(4):
+            n_r, n_c = cur_r+dr[direct], cur_c+dc[direct]
+            if (n_r < 0 or n_r >= R) or (n_c < 0 or n_c >= C) \
+                    or graph[n_r][n_c] != '.' or visited[n_r][n_c][0] <= time + 1 \
+                    or visited[n_r][n_c][1] == 1:
+                continue
+            queue.append([n_r, n_c, time+1])
+            visited[n_r][n_c][1] = 1
+    return result
+
+
+def fire_bfs(cur_list):
+    queue = deque(cur_list)
+    while queue:
+        cur_r, cur_c, time = queue.popleft()
+        for direct in range(4):
+            n_r, n_c = cur_r+dr[direct], cur_c+dc[direct]
+            if (n_r < 0 or n_r >= R) or (n_c < 0 or n_c >= C) \
+                    or graph[n_r][n_c] == '#' or visited[n_r][n_c][0] <= time + 1:
+                continue
+            queue.append([n_r, n_c, time+1])
+            visited[n_r][n_c][0] = time + 1
+
+
+if __name__ == "__main__":
+    import sys
+    from collections import deque
+
+    input = sys.stdin.readline
+
+    R, C = map(int, input().split())
+    graph = []
+    j_list = []
+    fire_list = []
+    dr, dc = [1, -1, 0, 0], [0, 0, 1, -1]
+    for r in range(R):
+        temp = list(input().rstrip())
+        for c in range(C):
+            cur_val = temp[c]
+            if cur_val == 'J':
+                j_list.append([r, c, 0])
+            elif cur_val == 'F':
+                fire_list.append([r, c, 0])
+        graph.append(temp)
+
+    visited = [[[float('inf'), 0] for _ in range(C)]
+               for _ in range(R)]
+    # index 0 -> 불 1 -> 지훈
+
+    fire_bfs(fire_list)
+    result = j_bfs(j_list)
+    if result != 0:
+        print(result)
+    else:
+        print("IMPOSSIBLE")
+
+# 첫 번째 풀이 속도 : 1980ms
+
+
 def bfs():
     from collections import deque
     queue = deque(fire_start + j_start)
