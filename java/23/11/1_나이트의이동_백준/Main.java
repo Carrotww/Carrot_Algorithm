5ee0f5e1c3c1ce4ee1fbf33f43d1f49f68613247
Carrotww <rjgjfl90@naver.com>
2023-11-03 17:17:49 +0900

    나이트의 이동 java 풀이

--- java/23/11/1_나이트의이동_백준/Main.java
+++ java/23/11/1_나이트의이동_백준/Main.java
@@ -28,16 +28,46 @@ public class Main {
         target_r = Integer.parseInt(targetValue[0]);
         target_c = Integer.parseInt(targetValue[1]);
 
-        bfs(cur_r, cur_c);
+        System.out.println(bfs(cur_r, cur_c));
     }
 
     private static int bfs(int r, int c) {
         Queue<int[]> queue = new LinkedList<>();
         int[][] visited = new int[n][n];
+        int[][] moves = {
+            {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2},
+            {1, 2}, {2, 1}, {2, -1}, {1, -2}
+        };
         for (int i = 0; i < n; i++) {
             Arrays.fill(visited[i], -1);
         }
+        queue.offer(new int[]{r, c});
+        if ((r == target_r) && (c == target_c)) {
+            return 0;
+        }
+        int result = 0;
+        visited[r][c] = 0;
 
-        return 0;
+        while (queue.size() > 0) {
+            int[] cur_val = queue.poll();
+            int cur_r = cur_val[0];
+            int cur_c = cur_val[1];
+
+            for (int[] move : moves) {
+                int n_r = cur_r + move[0];
+                int n_c = cur_c + move[1];
+                if (n_r == target_r && n_c == target_c) {
+                    return visited[cur_r][cur_c] + 1;
+                }
+                if ((n_r < 0 || n_r >= n) || (n_c < 0 || n_c >= n) || (visited[n_r][n_c] != -1)) {
+                    continue;
+                } else {
+                    visited[n_r][n_c] = visited[cur_r][cur_c] + 1;
+                    queue.offer(new int[] {n_r, n_c});
+                }
+            }
+        }
+        return -1;
     }
 }
+
