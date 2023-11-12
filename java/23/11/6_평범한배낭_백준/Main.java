03c3c4f357957dd20ef63ba49b92aad529873de6
Carrotww <rjgjfl90@naver.com>
2023-11-13 00:24:22 +0900

    temp

---
+++ java/23/11/6_평범한배낭_백준/Main.java
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/12865
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        w = Integer.parseInt(st.nextToken());
+
+    }
