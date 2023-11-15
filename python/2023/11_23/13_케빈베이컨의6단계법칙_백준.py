399661952c0185bc9924114b7b15b141a82d5a09
Carrotww <rjgjfl90@naver.com>
2023-11-15 15:19:29 +0900

    케빈베이컨

--- python/2023/11_23/13_케빈베이컨의6단계법칙_백준.py
+++ python/2023/11_23/13_케빈베이컨의6단계법칙_백준.py
@@ -2,18 +2,32 @@
 
 if __name__ == "__main__":
     import sys
+    from collections import deque, defaultdict
+
     input = sys.stdin.readline
 
     n, m = map(int, input().split())
-    graph = dict()
-    for _ in range(n):
+    graph = defaultdict(list)
+    for _ in range(m):
         start, end = map(int, input().split())
-        if start in graph:
-            graph[start].append(end)
-        else:
-            graph[start] = [end]
-        if end in graph:
-            graph[end].append(start)
-        else:
-            graph[end] = [start]
+        graph[start].append(end)
+        graph[end].append(start)
 
+    result = [0] * (n+1)
+    for i in range(1, n+1):
+        r = [-1] * (n + 1)
+        r[i] = 0
+        queue = deque([i])
+
+        while queue:
+            node = queue.popleft()
+            for n_node in graph[node]:
+                if r[n_node] == -1 or r[n_node] > r[node] + 1:
+                    r[n_node] = r[node] + 1
+                    queue.append(n_node)
+        result[i] = sum(r)
+    result[0] = float('inf')
+    for i in range(1, n+1):
+        if result[i] == min(result):
+            print(i)
+            break
