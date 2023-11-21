274b36881dc7fbd3d00ac5e8bc293840f3c016a5
Carrotww <rjgjfl90@naver.com>
2023-11-22 01:52:19 +0900

    백준 빗물

---
+++ python/2023/11_23/21_빗물_백준.py
@@ -0,0 +1,21 @@
+# https://www.acmicpc.net/problem/14719
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.readline
+
+    h, w = map(int, input().split())
+    ary = list(map(int, input().split()))
+
+    stack = []
+    result = 0
+    for i in range(w):
+        while stack and ary[i] > ary[stack[-1]]:
+            top = stack.pop()
+            if not stack:
+                break
+            distance = (i - stack[-1] - 1)
+            height = min(ary[i], ary[stack[-1]]) - ary[top]
+            result += (distance * height)
+        stack.append(i)
+    print(result)
