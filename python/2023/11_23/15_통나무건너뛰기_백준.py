e7760853e7b52e8f3140fc079854aab642a3d4d5
Carrotww <rjgjfl90@naver.com>
2023-12-20 21:41:34 +0900

    통나무

---
+++ python/2023/11_23/15_통나무건너뛰기_백준.py
@@ -0,0 +1,17 @@
+# https://www.acmicpc.net/problem/11497
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.readline
+
+    t = int(input())
+    result = []
+    for _ in range(t):
+        n = int(input()
+        ary = list(map(int, input().split()))
+
+
+
+    # 결과 출력
+    for val in result:
+        print(val)
