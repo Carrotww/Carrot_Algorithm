7069edff2b56a8d2e29dc558308f00c0f1746a5d
Carrotww <rjgjfl90@naver.com>
2023-12-20 23:54:24 +0900

    줄세우기(위상정렬), 소수구하기 풀이

---
+++ python/2023/12_23/20_2줄세우기.py
@@ -0,0 +1,41 @@
+# https://www.acmicpc.net/problem/2252
+
+if __name__ == "__main__":
+    from collections import deque
+    import sys
+    input = sys.stdin.readline
+
+    graph = dict()
+    topology = dict()
+
+    n, m = map(int, input().split())
+    for _ in range(m):
+        a, b = map(int, input().split())
+        if a in graph:
+            graph[a].append(b)
+        else:
+            graph[a] = [b]
+        if b in topology:
+            topology[b] += 1
+        else:
+            topology[b] = 1
+
+    queue = deque()
+    for i in range(1, n+1):
+        if i not in topology:
+            queue.append(i)
+
+    result = []
+    while queue:
+        cur_node = queue.popleft()
+        result.append(cur_node)
+        
+        if cur_node in graph:
+            for i in graph[cur_node]:
+                if i in topology:
+                    topology[i] -= 1
+                    if topology[i] == 0:
+                        queue.append(i)
+            graph[cur_node] = []
+    print(' '.join([str(x) for x in result]))
+
