1a3d91c2ff17e129683ff9e38a11adace8b6a787
Carrotww <rjgjfl90@naver.com>
2024-03-03 20:13:21 +0900

    백준 가장긴증가하는부분수열4

---
+++ python/2024/2/29_가장긴증가하는부분수열4.py
@@ -0,0 +1,32 @@
+# https://www.acmicpc.net/problem/14002
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.readline
+
+    n = int(input())
+    ary = list(map(int, input().split()))
+
+    dp = [0] * n
+    dp[0] = 1
+
+    for i in range(1, n):
+        dp[i] = 1
+        for j in range(i):
+            if ary[i] > ary[j]:
+                dp[i] = max(dp[i], dp[j]+1)
+
+    result = max(dp)
+    max_idx = dp.index(result)
+    result_list = [ary[max_idx]]
+    
+    temp = result
+    for i in range(max_idx, -1, -1):
+        if dp[i] == temp - 1:
+            temp -= 1
+            result_list.append(ary[i])
+
+    print(result)
+    print(' '.join(map(str, sorted(result_list))))
+
+
--- python/2024/2/가장긴증가하는부분수열4.py
+++ python/2024/2/가장긴증가하는부분수열4.py
@@ -1,2 +1,31 @@
 # https://www.acmicpc.net/problem/14002
 
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.readline
+
+    n = int(input())
+    ary = list(map(int, input().split()))
+
+    dp = [[0] * n for _ in range(n)]
+
+    dp = [0] * n
+    result = 0
+    result_list = []
+    for i in range(n):
+        temp = ary[i]
+        temp_list = [temp]
+        cnt = 1
+        for j in range(i+1, n):
+            if temp < ary[j]:
+                cnt += 1
+                temp = ary[j]
+                temp_list.append(temp)
+        if cnt > result:
+            result = cnt
+            result_list = temp_list
+
+    print(result)
+    print(' '.join(map(str, result_list)))
+
+
