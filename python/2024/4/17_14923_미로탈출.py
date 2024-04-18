674fcb1385c5ca4d4d6bc146e77e306e197ddd2f
Carrotww <rjgjfl90@naver.com>
2024-04-18 13:16:35 +0900

    미로탈출 풀이

--- python/2024/4/17_14923_미로탈출.py
+++ python/2024/4/17_14923_미로탈출.py
@@ -13,12 +13,31 @@ if __name__ == "__main__":
     for _ in range(r):
         graph.append(list(map(int, input().split())))
 
-    visited = [[[0] for _ in range(c)] for _ in range(r)]
+    visited = [[[0]*2 for _ in range(c)] for _ in range(r)]
+    visited[start_r-1][start_c-1][0] = 1
     dr, dc = [0, 1, 0, -1], [1, 0, -1, 0]
+    result = float('inf')
 
-    queue = deque([(start_r, start_c, 0, 0)])
+    queue = deque([(start_r-1, start_c-1, 0, 0)])
     while queue:
         cur_r, cur_c, is_broke, cnt = queue.popleft()
-
-
+        if (cur_r, cur_c) == (target_r-1, target_c-1):
+            result = cnt
+            break
+        for d in range(4):
+            n_r, n_c = dr[d]+cur_r, dc[d]+cur_c
+            if (n_r < 0 or n_r >= r) or (n_c < 0 or n_c >= c):
+                continue
+            if visited[n_r][n_c][is_broke] == 1:
+                continue
+            if graph[n_r][n_c] == 1 and not is_broke:
+                queue.append((n_r, n_c, 1, cnt+1))
+                visited[n_r][n_c][1] = 1
+            elif graph[n_r][n_c] == 0:
+                queue.append((n_r, n_c, is_broke, cnt+1))
+                visited[n_r][n_c][is_broke] = 1
+    if result == float('inf'):
+        print(-1)
+    else:
+        print(result)
 
