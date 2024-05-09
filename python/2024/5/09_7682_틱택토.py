98d5e1174076c5eedb283a3ebadb7f307ed60b69
Carrotww <rjgjfl90@naver.com>
2024-05-09 19:29:13 +0900

    백준 틱택토 푸는중

---
+++ python/2024/5/09_7682_틱택토.py
@@ -0,0 +1,110 @@
+# https://www.acmicpc.net/problem/7682
+
+def checkbingo(player, graph):
+    # 가로 체크
+    for i in range(3):
+        cnt = 0
+        for j in range(3):
+            if graph[i][j] == player:
+                cnt += 1
+        if cnt == 3:
+            return True
+
+    # 세로 체크
+    for i in range(3):
+        cnt = 0
+        for j in range(3):
+            if graph[j][i] == player:
+                cnt += 1
+        if cnt == 3:
+            return True
+
+    # 대각선 체크
+    start_r, start_c = 0, 0
+    cnt = 0
+    for i in range(3):
+        if graph[start_r + i][start_c + i] == player:
+            cnt += 1
+    if cnt == 3:
+        return True
+
+    start_r, start_c = 0, 2
+    cnt = 0
+    for i in range(3):
+        if graph[start_r + i][start_c - i] == player:
+            cnt += 1
+    if cnt == 3:
+        return True
+
+    return False
+
+
+def solve(t):
+    graph = []
+    start = 0
+    for _ in range(3):
+        graph.append(t[start:start + 3])
+        start += 3
+
+    cur_o, cur_x, cur_b = 0, 0, 0
+    for i in range(3):
+        for j in range(3):
+            if graph[i][j] == 'O':
+                cur_o += 1
+            elif graph[i][j] == 'X':
+                cur_x += 1
+            else:
+                cur_b += 1
+
+    if cur_o > cur_x:
+        return False
+
+    if cur_x - cur_o > 1:
+        return False
+
+    if checkbingo('O', graph) and not checkbingo('X', graph):
+        if cur_x != cur_o:
+            return False
+
+    if checkbingo('O', graph) and checkbingo('X', graph):
+        return False
+        
+
+    if cur_o > cur_x or not cur_o + 1 >= cur_x:
+        return False
+
+
+    if cur_o <= cur_x:
+        # 'O'가 빙고를 달성했는지 확인하는 로직
+        if checkbingo('O', graph):
+            return False
+
+    if cur_o == cur_x and cur_b:
+        if checkbingo('X', graph):
+            return False
+
+    # 빙고 상태를 확인하는 로직
+    if not checkbingo('O', graph) and not checkbingo('X', graph) and cur_b:
+        return False
+
+    return True
+
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.readline
+    graph = []
+    result = []
+
+    while True:
+        t = list(input().strip())
+        if t[0] == 'e':
+            break
+        if solve(t):
+            result.append('valid')
+        else:
+            result.append('invalid')
+
+    for r in result:
+        print(r)
+
