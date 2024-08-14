a559fd2579643d479d8305a3233782d23bd24bb4
Carrotww <rjgjfl90@naver.com>
2024-08-14 16:24:23 +0900

    t

---
+++ python/2024/8/14_백준_도시분할계획.py
@@ -0,0 +1,11 @@
+# https://www.acmicpc.net/problem/1647
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.readline
+
+    n, m = map(int, input().split())
+    # 마을을 두 개로 나누고
+
+
+
