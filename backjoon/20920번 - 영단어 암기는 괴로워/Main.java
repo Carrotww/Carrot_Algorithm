cc7251cbf6256e5ec18fbf5986497ac092532c1f
Carrot_hyeong <rjgjfl90@naver.com>
2025-08-26 22:36:57 +0900

    올림픽 init

--- backjoon/20920번 - 영단어 암기는 괴로워/Main.java
+++ backjoon/20920번 - 영단어 암기는 괴로워/Main.java
@@ -15,6 +15,36 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            if (line.length() >= M) {
+                map.put(line, map.getOrDefault(line, 0) + 1);
+            }
+        }
+
+        List<Map.Entry<String, Integer>> entryList = new ArrayList<>(map.entrySet());
+
+        entryList.sort((a, b) -> {
+            if (a.getValue() != b.getValue()) {
+                return Integer.compare(b.getValue(), a.getValue());
+            }
+
+            if (a.getKey().length() != b.getKey().length()) {
+                return Integer.compare(b.getKey().length(), a.getKey().length());
+            }
+
+            return a.getKey().compareTo(b.getKey());
+        });
+
+        for (Map.Entry<String, Integer> entry : entryList) {
+            System.out.println(entry.getKey());
+        }
     }
 }
---
+++ backjoon/8979번 - 올림픽/Main.java
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 8979                              :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: rjgjfl <boj.kr/u/rjgjfl>                    +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/8979                           #+#        #+#      #+#    */
+/*   Solved: 2025/08/26 22:29:25 by rjgjfl        ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        input();
+    }
+
+    public static void input() throws IOException {
+
+    }
+}
