68ddae54ec6afe828d7213fa103e92f20a8d7fd2
Carrotww <rjgjfl90@naver.com>
2025-08-27 17:59:42 +0900

    올림픽 ing

--- backjoon/20920번 - 영단어 암기는 괴로워/Main2.java
+++ backjoon/20920번 - 영단어 암기는 괴로워/Main2.java
@@ -1,36 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                      :::    :::    :::     */
-/*   Problem Number: 8979                              :+:    :+:      :+:    */
+/*   Problem Number: 20920                             :+:    :+:      :+:    */
 /*                                                    +:+    +:+        +:+   */
 /*   By: rjgjfl <boj.kr/u/rjgjfl>                    +#+    +#+          +#+  */
 /*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/8979                           #+#        #+#      #+#    */
-/*   Solved: 2025/08/26 22:29:25 by rjgjfl        ###          ###   ##.kr    */
+/*   https://boj.kr/20920                          #+#        #+#      #+#    */
+/*   Solved: 2025/08/06 17:36:31 by rjgjfl        ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
 
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    static int N;
-    static int target;
-
+public class Main2 {
     public static void main(String[] args) throws IOException {
-        input();
-    }
-
-    public static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        N = Integer.parseInt(st.nextToken());
-        target = Integer.parseInt(st.nextToken());
+        Map<String, Integer> map = new HashMap<>();
 
         for (int i = 0; i < N; i++) {
             String line = br.readLine();
-
+            if (line.length() >= M) {
+                map.put(line, map.getOrDefault(line, 0) + 1);
+            }
         }
+
+        // 자주 나오는 단어일수록 앞에 배치한다.
+        // 해당 단어의 길이가 길수록 앞에 배치한다.
+        // 알파벳 사전 순으로 앞에 있는 단어일수록 앞에 배치한다
+
+        List<String> ary = new ArrayList<>(map.keySet());
     }
 }
