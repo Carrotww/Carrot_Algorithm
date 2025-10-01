eb10a330b0131105224a751b15bbbd65abad254f
Carrotww <rjgjfl90@naver.com>
2025-10-01 13:42:33 +0900

    뱀과 사다리 게임

--- backjoon/16928번 - 뱀과 사다리 게임/Main.java
+++ backjoon/16928번 - 뱀과 사다리 게임/Main.java
@@ -17,11 +17,10 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[][] graph;
-    static int[][] visited; // -1 init
     static Map<Integer, Integer> map;
-    static Queue<Integer> q;
-    static int cnt;
+    static int[] visited;
+    static int[] jump;
+    static Queue<int[]> q;
     static int N;
     static int M;
 
@@ -32,54 +31,46 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        map = new HashMap<>();
-        graph = new int[100][100];
         q = new LinkedList<>();
+        visited = new int[101];
+        jump = new int[101];
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < N + M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            map.put(a, b);
+            jump[a] = b;
         }
 
-        int value = 1;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                graph[i][j] = value;
-                value++;
-            }
-        }
-
-        for (int[] v : visited) {
-            Arrays.fill(v, -1);
-        }
-
-        q.add(1);
-
-        int cnt = 0;
+        q.add(new int[] { 1, 0 });
 
         while (!q.isEmpty()) {
-            cnt++;
 
-            int cur = q.poll();
+            int[] t = q.poll();
+            int cur = t[0];
+            int cnt = t[1];
 
             if (cur == 100) {
                 break;
             }
 
-            boolean go = false;
-
             for (int i = 1; i <= 6; i++) {
-                int next =  cur + i;
-            }
+                int next = cur + i;
+                
+                if (next > 100) continue;
 
-            if (!go) {
-                // 6번 가기
+                if (jump[next] != 0) {
+                    next = jump[next];
+                }
+
+                if (visited[next] == 0) {
+                    q.add(new int[] {next, cnt + 1});
+                    visited[next] = cnt + 1;
+                }
             }
         }
 
-        System.out.println(cnt);
+        System.out.println(visited[100]);
     }
 }
