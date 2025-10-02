1d5e3a11b911c5697cc70c7c58dea918db8554bd
Carrotww <rjgjfl90@naver.com>
2025-10-02 18:00:12 +0900

    경로찾기 init

--- backjoon/1916번 - 최소비용 구하기/Main2.java
+++ backjoon/1916번 - 최소비용 구하기/Main2.java
@@ -13,10 +13,12 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        final int maxValue = Integer.MAX_VALUE;
 
         int N = Integer.parseInt(st.nextToken());
 
@@ -24,8 +26,9 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
 
         List<int[]>[] graph = new ArrayList[N + 1];
-        int[] visited = new int[N + 1];
-        Arrays.fill(visited, -1);
+        int[] dist = new int [N + 1];
+        boolean[] visited = new boolean[N + 1];
+        Arrays.fill(dist, maxValue);
 
         for (int i = 1; i < N + 1; i++)
             graph[i] = new ArrayList<>();
@@ -48,36 +51,33 @@ public class Main {
 
         pq.add(new int[] { 0, a });
 
-        visited[a] = 0;
+        dist[a] = 0;
 
         while (!pq.isEmpty()) {
-            // poll pq
             int[] node = pq.poll();
-            int cur = node[1];
             int cnt = node[0];
+            int cur = node[1];
 
-            // if not same visited value = continue
-            if (visited[cur] != cnt) continue;
+            // queue를 통과했는데 최소가 아니라면
+            if (visited[cur]) continue;
+            visited[cur] = true;
 
             if (cur == b) {
-                System.out.println(visited[cur]);
+                System.out.println(dist[b]);
                 return;
             }
 
-            // iterator graph next value
-            // check next value and visited
+            for (int[] nextNode : graph[cur]) {
+                int next = nextNode[0];
+                int nextCnt = nextNode[1];
 
-            for (int[] nNode : graph[cur]) {
-                int next = nNode[0];
-                int nCnt = nNode[1];
-
-                if (visited[next] == -1 || visited[next] > cnt + nCnt) {
-                    pq.add(new int[] {cnt + nCnt, next});
-                    visited[next] = cnt + nCnt;
+                if (!visited[next] && dist[next] > nextCnt + cnt) {
+                    dist[next] = nextCnt + cnt;
+                    pq.add(new int[] {nextCnt + cnt, next});
                 }
             }
         }
 
-        System.out.println(visited[b]);
+        System.out.println(dist[b]);
     }
 }
